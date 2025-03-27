6b01001ba064f12135faf033a4c675e3b92a6f51
Chris Hoy <15720741+chrishoy@users.noreply.github.com>
2025-03-27 14:20:36 +0000

    Version 4 Tailwind

--- ollamachatclientexample.client/src/App.tsx
+++ ollamachatclientexample.client/src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+﻿import { useState } from 'react';
 import { Button } from "./components/ui/Button";
 import { Textarea } from "./components/ui/Textarea";
 import './App.css';
+import ChatComponent from './components/ui/ChatComponent';
 
 interface ChatResponse {
     id: string;
@@ -31,7 +32,7 @@ function App() {
     };
 
     const messageContents =
-        <div className="p-4 space-y-4">
+        <div className="space-y-4 p-4">
             <Textarea
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
@@ -40,7 +41,7 @@ function App() {
             />
                 <Button onClick={handleSubmit}>Submit</Button>
             {response && (
-                <div className="p-2 border rounded mt-2">
+                <div className="mt-2 rounded border p-2">
                     <p><strong>Id:</strong> {response.id}</p>
                     <p><strong>Role:</strong> {response.role}</p>
                     <p><strong>Message:</strong> {response.text}</p>
@@ -53,14 +54,12 @@ function App() {
         <div>
             <h1 id="tableLabel">Ollama Chat...</h1>
             <p>This component demonstrates the uses of the Ollama AI chat service.</p>
-
-            <div className="grid grid-flow-col grid-rows-3 gap-4">
-                <div className="row-span-3 bg-fuchsia-600 border-blue-500 border-2">Spans 3 rows</div>
-                <div className="col-span-2 border-red-500 border-2">Spans 2 cols</div>
-                <div className="col-span-2 row-span-2 border-green-500 border-2">Spans 2 cols and 2 rows</div>
+            <div className="gap=5 grid grid-flow-row">
+                <div>{messageContents}</div>
+                <div>
+                    <ChatComponent />
+                </div>
             </div>
-
-            <div>{messageContents}</div>
         </div>
     );
 }
